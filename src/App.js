301ba1959eca0936f609ac2e49eec7854682df10
Clara Lie <lieclara96@gmail.com>
2025-11-18 19:44:00 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const [isChecked, setIsChecked] = React.useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='form-login'>
+        <h1 className='header'>Login</h1>
+        <input 
+          className='input-user-login'
+          type='text' 
+          placeholder='Username'
+        ></input>
+        <input 
+          className='input-password-login'
+          type='password' 
+          placeholder='Password'
+        ></input>
+        <div className='checkbox-login'>
+          <input 
+            type='checkbox' 
+            checked={isChecked}
+            onChange={() => setIsChecked(!isChecked)}
+          ></input>
+          <label className='label-remember-me'>Remember me</label>
+        </div>
+        <div className='forgot-password'>
+          <a href='#' className='link-forgot-password'>Forgot Password?</a>
+        </div>
+        <div>
+          <button className='button'>Login</button>
+        <div className='sign-up'>
+          <label className='label-remember-me'>Don't have an Account?</label>&nbsp;
+          <a href='#' className='link-forgot-password'>Register</a>
+        </div>
+        </div>
+      </div>
     </div>
   );
 }

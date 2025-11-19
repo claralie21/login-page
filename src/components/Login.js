72db026b46a7490b6a93740771f05f56afb5a83f
Clara Lie <lieclara96@gmail.com>
2025-11-19 13:22:27 +0700

    add handleLogin function

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,16 +6,48 @@ function Login() {
   const [isChecked, setIsChecked] = React.useState(false);
   const [username, setUsername] = React.useState('');
   const [password, setPassword] = React.useState('');
+  const [errorMessage, setErrorMessage] = React.useState(null);
+  const [successMessage, setSuccessMessage] = React.useState(null);
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+  const [isLoggingIn, setIsLoggingIn] = React.useState(false);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setErrorMessage(null);
+    setIsLoggedIn(false)
+    setIsLoggingIn(true);
+    try {
+      const baseUrl = 'https://example.com/api/login'
+      const response =  baseUrl && await fetch(baseUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+      if (response.ok) {
+        setIsLoggedIn(true);
+        setSuccessMessage('Login successful!');
+      }
+    }
+    catch (error) {
+      setErrorMessage('Login failed. Please try again.');
+    } finally {
+      setIsLoggingIn(false);
+      setIsLoggedIn(true)
+    }
+  }
 
   return (
     <div className="App">
-      <div className='form-login'>
+      <form className='form-login'>
         <h1 className='header'>Login</h1>
         <input 
           className='input-user-login'
           type='text' 
           placeholder='Username'
           value={username}
+          onChange={e => setUsername(e.target.value)}
           required
         ></input>
         <input 
@@ -23,6 +55,7 @@ function Login() {
           type='password' 
           placeholder='Password'
           value={password}
+          onChange={e => setPassword(e.target.value)}
           required
         ></input>
         <div className='checkbox-login'>
@@ -36,14 +69,18 @@ function Login() {
         <div className='forgot-password'>
           <a href='/forgot-password' className='link-forgot-password'>Forgot Password?</a>
         </div>
+        {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+        {successMessage && isLoggedIn && <p style={{ color: 'green' }}>{successMessage}</p>}
         <div>
-          <button className='button' type='submit' disabled={!username || !password}>Login</button>
+          <button className='button' type='submit' disabled={isLoggingIn || !username || !password} onClick={handleLogin}>
+            {isLoggingIn ? 'Logging in...' : 'Log In'}
+          </button>
           <div className='sign-up'>
             <label className='label-remember-me'>Don't have an account?</label>&nbsp;
             <Link to="/signup">Register</Link>
           </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
